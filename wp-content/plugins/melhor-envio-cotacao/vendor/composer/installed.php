8a59b55f5bacbaaa1d9f54e8d8bb89ecd322559c
wp-server <vinicius020997@gmail.com>
2025-01-20 18:17:09 -0300

    Comitando arquivos para compartilhar no GitHub e averiguar códigos de plugins e fazer correções se necessário.

---
+++ wp-content/plugins/melhor-envio-cotacao/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'melhorenvio/melhorenvio-cotation',
+        'pretty_version' => 'v2.15.9',
+        'version' => '2.15.9.0',
+        'reference' => '6e2f5bb01c536df9fc84534eb8a27ec99d9601af',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'melhorenvio/melhorenvio-cotation' => array(
+            'pretty_version' => 'v2.15.9',
+            'version' => '2.15.9.0',
+            'reference' => '6e2f5bb01c536df9fc84534eb8a27ec99d9601af',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
